3b60965ee235f59860741e3bd7e87214dbe97139
dukeqiu <duke.qiu@lmxmn012.rcoffice.ringcentral.com>
2019-01-29 17:16:51 +0800

    add config sample

---
+++ config.sample.js
@@ -0,0 +1,23 @@
+let config = null
+config = {
+    testenv_itl: '',
+    rc_username : '',
+    rc_password:'',
+
+    dropbox_username: '',
+    dropbox_password: '',
+
+    googledrive_username: '',
+    googledrive_password: '',
+
+    box_username: '',
+    box_password: '',
+
+    sftp_host:'',
+    sftp_port:'',
+    sftp_username:'',
+    sftp_password:'',
+
+}
+
+module.exports = config;
